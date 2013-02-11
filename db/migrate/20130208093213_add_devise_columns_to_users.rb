c6a5b4a0df6a286b9dd0e311f410707a8eadcae5
mayank <mjain@grepruby.com>
2013-02-11 15:33:11 +0530

    edit add user column

--- db/migrate/20130208093213_add_devise_columns_to_users.rb
+++ db/migrate/20130208093213_add_devise_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddDeviseColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string, :null => false, :default => ""
     add_column :users, :gender, :string, :null => false, :default => ""
-    add_column :users, :dob, :date, :null => false, :default => ""
+    add_column :users, :dob, :date
   end
 end
