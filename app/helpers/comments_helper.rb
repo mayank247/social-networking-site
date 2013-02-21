c0013ff8726a6235394a45bfb2ad9679d2ab57e5
mayank <mjain@grepruby.com>
2013-02-21 16:22:57 +0530

    Use Helpers and shift view code to helpers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+
+  def count_like_on_comment(comment)
+    comment.likes.count(:all)
+  end
+
+  def check_like_on_comment(comment)
+    comment.likes.where("user_id=?", current_user.id)[0]
+  end
+
 end
--- app/helpers/status_updates_helper.rb
+++ app/helpers/status_updates_helper.rb
@@ -1,2 +1,11 @@
 module StatusUpdatesHelper
+  
+  def count_like_on_status(status_update)
+    status_update.likes.count(:all)
+  end
+
+  def check_like_on_status(status_update)
+    status_update.likes.where(:user_id => current_user.id)[0]
+  end
+  
 end
